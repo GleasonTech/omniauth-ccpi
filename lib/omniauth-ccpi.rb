4dc7d284d489808cfb1de67a65e641c1e84c8b13
Nicholas W. Watson <nicholas.w.watson@me.com>
2016-12-05 16:06:06 -0500

    renamed file

--- lib/omniauth-ccpi.rb
+++ lib/omniauth-ccpi.rb
@@ -13,10 +13,10 @@ module OmniAuth
 
     # Sets the omniauth application id
     mattr_accessor :omniauth_entropi_app_id
-    @@omniauth_entropi_app_id = ""
+    @@omniauth_ccpi_app_id = ""
 
     mattr_accessor :omniauth_entropi_app_secret
-    @@omniauth_entropi_app_secret = ""
+    @@omniauth_ccpi_app_secret = ""
 
     def self.setup
       yield self

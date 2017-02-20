17bcaebef52fc591eb4fb4d65422689db62057e7
Nicholas W. Watson <nicholas.w.watson@me.com>
2017-02-20 16:17:43 -0500

    wtf

--- app/models/omni_auth/ccpi/organization.rb
+++ app/models/omni_auth/ccpi/organization.rb
@@ -1,12 +1,5 @@
 class OmniAuth::Ccpi::Organization < ActiveRecord::Base
-  self.site = "#{OmniAuth::Ccpi.provider_url}/api"
-
-  self.primary_key = :uid
-
-  self.user     = OmniAuth::Ccpi.omniauth_ccpi_app_id
-  self.password = OmniAuth::Ccpi.omniauth_ccpi_app_secret
-
   def users
-    
+
   end
 end
